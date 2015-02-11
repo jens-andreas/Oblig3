b356926b6872e0fde9fc8449708b2f484b70e7ce
Jens-Andreas Rensaa <jens-andreas@hotmail.com>
2015-02-11 09:13:09 +0100

    Kortoppgave Oblig 3

---
+++ utenforOneDrive/src/kort.java
@@ -0,0 +1,37 @@
+/**kort-klassen brukes for å lagre og administerere kortinformasjon */
+public abstract class kort {
+
+	String navn;
+	short pin;
+	boolean sperret;
+	int aksessKode;
+	
+	/**Konstruktør. Setter navn og pin til gitte verdeier.
+	 * Sier at kortet ikke er sperret.
+	 * Genererer en tilfeldig aksesskode*/
+	kort(String navn, short pin){
+		this.navn = navn;
+		this.pin = pin;
+		sperret = false;
+		aksessKode = (int)(Math.random() * 99999999);
+	}
+	
+	/** Returnerer navnet på kortholderen */
+	public String getNavn(){
+		return navn;
+	}
+	
+	/**Returnerer om kortet er sperret eller ikke.*/
+	public boolean isSperret(){
+		return sperret;
+	}
+	
+	public String toString(){
+		return  "Navn:       " + navn +
+				"\nPin :       " + pin  +
+				"\nSperret:    " + sperret +
+				"\nAksesskode: " + aksessKode;
+	}
+	
+	public abstract boolean sjekkPin(short pin);
+}
