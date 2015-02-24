04ab9dd801256ea015e132f63c20de9c25c43aa3
Jens-Andreas Rensaa <jens-andreas@hotmail.com>
2015-02-24 12:52:25 +0100

    V0.2

--- utenforOneDrive/src/ansatt.java
+++ utenforOneDrive/src/ansatt.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+
 /**Ansatt utvider kort.
  * Klassen lagrer og administerer data om ansattes kort*/
 public class ansatt extends kort {
@@ -7,6 +9,22 @@ public class ansatt extends kort {
 	}
 	
 	public boolean sjekkPin(int pin){
-		return pin == this.pin;
+		return (pin == this.pin);
+	}
+	
+	public boolean sjekkTid(){
+		Calendar tid = Calendar.getInstance();
+		return (tid.HOUR_OF_DAY > 7) && (tid.HOUR_OF_DAY < 17);
+	}
+	
+	public boolean brukKort(){
+		return brukKort(0000);
+	}
+	
+	public boolean brukKort(int pin){
+		boolean rettPin = sjekkPin(pin);
+		boolean sperret = isSperret();
+		boolean tidOK 	= sjekkTid();
+		return !sperret && (tidOK || rettPin);
 	}
 }
--- utenforOneDrive/src/kort.java
+++ utenforOneDrive/src/kort.java
@@ -1,4 +1,5 @@
-/**kort-klassen brukes for å lagre og administerere kortinformasjon */
+/**kort-klassen brukes for å lagre og administerere kortinformasjon.
+ * Klassen er abstrakt og skal brukes av underklasser. */
 public abstract class kort {
 
 	String navn;
@@ -26,6 +27,17 @@ public abstract class kort {
 		return sperret;
 	}
 	
+	/** Sperrer ett kort */
+	public void sperrKort(){
+		sperret = true;
+	}
+	
+	/** Åpner ett kort */
+	public void opneKort(){
+		sperret = false;
+	}
+	
+	/**Returnerer en streng med all data i kort-klassen*/
 	public String toString(){
 		return  "Navn:       " + navn +
 				"\nPin :       " + pin  +
@@ -33,5 +45,8 @@ public abstract class kort {
 				"\nAksesskode: " + aksessKode;
 	}
 	
+	/** Abstrakt metode som skal brukes av underklassene*/
 	public abstract boolean sjekkPin(int pin);
+	
+	public abstract boolean brukKort(int pin);
 }
