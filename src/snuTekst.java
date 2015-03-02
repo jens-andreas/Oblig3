a4d1670ad930fcb1e1237895bfd7bcd4430d5261
Jens-Andreas Rensaa <jens-andreas@hotmail.com>
2015-03-02 12:38:27 +0100

    snuTekst V0.1

---
+++ src/snuTekst.java
@@ -0,0 +1,24 @@
+package snuTekst;
+import java.util.Scanner;
+
+public class snuTekst {
+	public static void main(String[] args){
+		
+			while(true){
+			Scanner input = new Scanner(System.in);
+			System.out.print("\nSkriv inn en streng: ");
+			String inputStreng = input.nextLine();
+			System.out.print("Strengen skrevet baklengs: ");
+			baklengs(inputStreng);
+			
+		}
+	}
+	
+	public static void baklengs(String tekst){
+		int lengde = tekst.length();
+		if(lengde > 0){
+			System.out.print(tekst.charAt(lengde - 1));
+			baklengs(tekst.substring(0, lengde - 1));
+		}
+	}
+}
