9c3e16b72fc7cfe306fbe81061cda12cf7c9112b
Jens-Andreas Rensaa <jens-andreas@hotmail.com>
2015-03-02 12:44:46 +0100

    snuTekst V0.2

--- src/snuTekst.java
+++ src/snuTekst.java
@@ -8,16 +8,24 @@ public class snuTekst {
 			System.out.print("\nSkriv inn en streng: ");
 			String inputStreng = input.nextLine();
 			System.out.print("Strengen skrevet baklengs: ");
-			baklengs(inputStreng);
+			baklengs(inputStreng, new int[256]);
 			
 		}
 	}
 	
-	public static void baklengs(String tekst){
+	public static void baklengs(String tekst, int[] charListe){
 		int lengde = tekst.length();
 		if(lengde > 0){
 			System.out.print(tekst.charAt(lengde - 1));
-			baklengs(tekst.substring(0, lengde - 1));
+			charListe[(int)(Character.toLowerCase(tekst.charAt(tekst.length() - 1)))]++;
+			baklengs(tekst.substring(0, lengde - 1),charListe);
+		}else{
+			for (int i = 0; i < charListe.length; i++){
+				if(charListe[i] > 0){
+					System.out.print("\nTegnet " + (char)i + " forekommer " + charListe[i] + " ganger.");
+				}
+			}
+			System.out.println("");
 		}
 	}
 }
