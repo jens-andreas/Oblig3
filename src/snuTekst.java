38259aaa8a9beaa5abb3d10955591938d68d4b5c
Jens-Andreas Rensaa <jens-andreas@hotmail.com>
2015-03-02 12:53:09 +0100

    snuTekst V0.3

--- src/snuTekst.java
+++ src/snuTekst.java
@@ -8,17 +8,21 @@ public class snuTekst {
 			System.out.print("\nSkriv inn en streng: ");
 			String inputStreng = input.nextLine();
 			System.out.print("Strengen skrevet baklengs: ");
-			baklengs(inputStreng, new int[256]);
+			baklengs(inputStreng);
 			
 		}
 	}
 	
-	public static void baklengs(String tekst, int[] charListe){
+	public static void baklengs(String tekst){
+		baklengs(tekst, new int[256], tekst.length());
+	}
+	
+	public static void baklengs(String tekst, int[] charListe, int siste){
 		int lengde = tekst.length();
-		if(lengde > 0){
-			System.out.print(tekst.charAt(lengde - 1));
-			charListe[(int)(Character.toLowerCase(tekst.charAt(tekst.length() - 1)))]++;
-			baklengs(tekst.substring(0, lengde - 1),charListe);
+		if(siste > 0){
+			System.out.print(tekst.charAt(siste - 1));
+			charListe[(int)(Character.toLowerCase(tekst.charAt(siste - 1)))]++;
+			baklengs(tekst,charListe,siste - 1);
 		}else{
 			for (int i = 0; i < charListe.length; i++){
 				if(charListe[i] > 0){
